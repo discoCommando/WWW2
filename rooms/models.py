b75d3bf709ec6e513a00d0c174f42b685670853b
Piotrus <piotrus@ubuntu.ubuntu-domain>
2014-05-09 09:03:20 +0200

    working i suppose

--- rooms/models.py
+++ rooms/models.py
@@ -24,7 +24,7 @@ class FreeDate(models.Model):
 	to_hour = models.IntegerField()
 	
 	def __unicode__(self):
-		return self.room.name + " " + str(self.from_hour) + "-" + str(self.to_hour);
+		return self.room.name + " " + str(self.from_hour) + "-" + str(self.to_hour) + " " + str(self.date);
 	
 	def save(self, force_insert=False, force_update=False, using=None):
 		fd_from = FreeDate.objects.all().filter(Q(date = self.date) & Q(room = self.room) & Q(from_hour__lte = self.from_hour) & Q(to_hour__gt = self.from_hour))
@@ -55,6 +55,9 @@ class FreeDate(models.Model):
 		same_date = FreeDate.objects.all().filter(Q(date = self.date))
 		ts_fr = same_date.filter(Q(from_hour = self.to_hour))
 		ts_t = same_date.filter(Q(to_hour = self.from_hour))
+		
+		if self.to_hour <= self.from_hour:
+			can_save = False
 		
 		if can_save & can_save2:
 			if ts_fr:
@@ -88,9 +91,15 @@ class Booking(models.Model):
 			roomz = room.first()
 			room.first().delete()
 			if roomz.from_hour < self.from_hour:
-				FreeDate.objects.create(date = roomz.date, room = roomz.room, from_hour = roomz.from_hour, to_hour = self.from_hour)
+				try:
+					FreeDate.objects.create(date = roomz.date, room = roomz.room, from_hour = roomz.from_hour, to_hour = self.from_hour)
+				except Exception:
+					roomz = roomz
 			if roomz.to_hour > self.to_hour:
-				FreeDate.objects.create(date = roomz.date, room = roomz.room, from_hour = self.to_hour, to_hour = roomz.to_hour)
+				try:
+					FreeDate.objects.create(date = roomz.date, room = roomz.room, from_hour = self.to_hour, to_hour = roomz.to_hour)
+				except Exception:
+					roomz = roomz
 			models.Model.save(self, force_insert, force_update, using)
 		
 # Create your models here.
